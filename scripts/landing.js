5f11dafa29475f4f208297b60ef992e9bd8119ae
Daniela Sannino <danielasannino@daniela-sanninos-macbook.local>
2017-03-02 22:35:09 -0500

    Checkpoint 9 Revision

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,65 +1,17 @@
-<<<<<<< HEAD
-var animatePoints = function() {
-    
-    var points = document.getElementsByClassName('point');
-    
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)"
-    };
-    
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-};
-=======
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
- 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
- };
-
+var animatePoints = function(points) {
+    for (i = 0; i < points.length; i++) {
+         var revealPoint = function(i) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+        revealPoint(i);
+    }
+};
+            
 window.onload = function () {
     
     if (window.innerHeight > 950) {
@@ -75,4 +27,4 @@ window.onload = function () {
         }
     });
 }
->>>>>>> checkpoint-9
+
